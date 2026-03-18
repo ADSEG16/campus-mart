40f50cdc9916447275ec39d2cef07334d0088262
Klenam <cedeegbe@gmail.com>
2026-03-18 17:29:05 +0000

    Auth Routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-const orderRoutes = require('./routes/order.routes');
-const adminRoutes = require('./routes/admin.routes');
-const errorHandler = require('./middleware/error.middleware');
+const express = require("express");
+const cors = require("cors");
+const orderRoutes = require("./routes/order.routes");
+const adminRoutes = require("./routes/admin.routes");
+const errorHandler = require("./middleware/error.middleware");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/orders', orderRoutes);
-app.use('/api/admin', adminRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/auth", authRoutes);
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ status: "ok" });
