21e70c904422dc2114b0603190e75b0eb1b2508c
Klenam <cedeegbe@gmail.com>
2026-03-18 17:25:30 +0000

    Auth Controller

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,40 @@
+const { findUserByEmail } = require("../services/userService");
+const jwt = require("jsonwebtoken");
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await findUserByEmail(email);
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    if (!user.isVerified) {
+      return res.status(403).json({ message: "User not verified" });
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = { loginUser };
